0c87caf5a5a9cfb9ea52251d195c94aee384479a
Adam Hemmings <adamhemmingsdev@gmail.com>
2025-07-30 17:00:51 +0100

    fix: exported supabase with default

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,4 +5,6 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabaseService = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey, supabaseService);
+const supabase = createClient(supabaseUrl, supabaseKey, supabaseService);
+
+export default supabase;
