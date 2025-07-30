7909a20c4a1fefab55e1a31fe536c582cd3b0933
Adam Hemmings <adamhemmingsdev@gmail.com>
2025-07-30 17:04:58 +0100

    fix: removed servicekey from createClient

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,8 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const supabaseService = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey, supabaseService);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
